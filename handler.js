b99f6558ba5b6835200c7ca58bfe4f7cbc91d231
Cesar Robles <cidrobles.cesar@gmail.com>
2021-10-23 19:50:35 -0500

    Unique Ingestion ID

--- handler.js
+++ handler.js
@@ -9,7 +9,7 @@ const AWS = require('aws-sdk');
 
 module.exports.main = serverless(app);
 
-module.exports.eventHandlerForATriggerCall  = async (event, context, callback) => {
+module.exports.eventHandlerForATriggerCall = async(event, context, callback) => {
     // console.log("start triggered event v: 0.0.3");
     // console.log("");
 
@@ -18,52 +18,51 @@ module.exports.eventHandlerForATriggerCall  = async (event, context, callback) =
     // console.log("end triggered event");
     // return {};
     console.log("start foreach");
-    for(let i = 0; i < event.Records.length; i++){
+    for (let i = 0; i < event.Records.length; i++) {
         let { body } = event.Records[i];
-        await controller.processSQSMessage({body: body});
+        await controller.processSQSMessage({ body: body });
     }
     console.log("end foreach");
     return {};
 };
-  
-module.exports.eventHandlerForADynamoTriggerCall  = async (event, context, callback) => {
+
+module.exports.eventHandlerForADynamoTriggerCall = async(event, context, callback) => {
     console.log("eventHandlerForADynamoTriggerCall");
     // console.log("event.Records",event.Records);
     console.log("start foreach");
-    for(let i = 0; i < event.Records.length; i++){
-        if(!event.Records[i].eventName.includes('REMOVE')){
-            console.log("event.Records[i]",event.Records[i].dynamodb);
-            console.log("event.Records[i].dynamodb.NewImage",event.Records[i].dynamodb.NewImage);
+    for (let i = 0; i < event.Records.length; i++) {
+        if (!event.Records[i].eventName.includes('REMOVE')) {
+            console.log("event.Records[i]", event.Records[i].dynamodb);
+            console.log("event.Records[i].dynamodb.NewImage", event.Records[i].dynamodb.NewImage);
             let data = event.Records[i].dynamodb.NewImage;
-            await controller.processDynamoMessage({body: data});
+            await controller.processDynamoMessage({ body: data });
         }
     }
     console.log("end foreach");
     return {};
 };
 
-module.exports.cron = function (event, context, callback) {
+module.exports.cron = function(event, context, callback) {
     var quicksight = new AWS.QuickSight();
 
-  return new Promise((resolve, reject) =>{
-    var params = {
-      AwsAccountId: process.env.AwsAccountId, // '327581952167', /* required */
-      DataSetId: process.env.DataSetId, // 'd639e92d-8c17-4497-a0c7-c8f93fb51a1f', /* required */
-      IngestionId: process.env.IngestionId, // 'TPV Data Ingestion', /* required */
-      // IngestionType: process.env.IngestionType // "INCREMENTAL_REFRESH"
-    };
-  
-    console.log("callQuicksight - params", params);
-  
-    quicksight.createIngestion(params, function (err, data) {
-      if (err){ 
-        console.log(err, err.stack); // an error occurred
-        reject(err);
-      }
-      else {
-        console.log("quicksight.createIngestion data",data); 
-        resolve(data);
-      }          // successful response
+    return new Promise((resolve, reject) => {
+        var params = {
+            AwsAccountId: process.env.AwsAccountId, // '327581952167', /* required */
+            DataSetId: process.env.DataSetId, // 'd639e92d-8c17-4497-a0c7-c8f93fb51a1f', /* required */
+            IngestionId: process.env.IngestionId + Date.now(), // 'TPV Data Ingestion', /* required */
+            // IngestionType: process.env.IngestionType // "INCREMENTAL_REFRESH"
+        };
+
+        console.log("callQuicksight - params", params);
+
+        quicksight.createIngestion(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                reject(err);
+            } else {
+                console.log("quicksight.createIngestion data", data);
+                resolve(data);
+            } // successful response
+        });
     });
-  });
 };
